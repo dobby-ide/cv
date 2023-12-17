4a5130e15eb09615cf0590a27155e54d829b6ef7
Fabio Privitera <fabio_privitera@yahoo.it>
2023-12-17 16:10:43 +0200

    sidebar with photos

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,19 @@
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/dist/ScrollTrigger';
+console.clear();
 gsap.registerPlugin(ScrollTrigger);
 console.log(gsap);
+let sections = gsap.utils.toArray('.photosidebar__card');
+console.log(sections);
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: 'none',
+  scrollTrigger: {
+    trigger: '.phosidebar',
+    pin: true,
+    scrub: 1,
+    snap: 1 / (sections.length - 1),
+    // base vertical scrolling on how wide the container is so it feels more natural.
+    end: '+=1000',
+  },
+});
